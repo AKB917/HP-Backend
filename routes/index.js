9c2101eadf64e4d482d6361ab88918a9fee5d4be
fabio fernandes <fabio.f@outlook.fr>
2025-02-09 19:12:54 +0100

    add character cards

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,35 @@ router.get('/spells', function (req, res, next) {
     })
 });
 
+router.get('/characters', function (req, res, next) {
+  fetch("https://hp-api.onrender.com/api/characters/students")
+    .then(response => response.json())
+    .then(data => {
+
+      let rand = Math.floor(Math.random() * data.length);
+      let tabnum = 0
+
+      if (data[rand].house === "") {
+        data[rand].house = "NEVER BEEN A HOGWARTS STUDENT"
+      }
+      if (data[rand].image === "") {
+        data[rand].image = "../images/logo.png"
+      }
+
+      const pattern = /[0-9]/g;
+      const charNumbers = data[rand].id.match(pattern)
+
+      for (let i of charNumbers) {
+        tabnum += (parseInt(i))
+      }
+      let character = {
+        name: data[rand].name,
+        house: data[rand].house,
+        image: data[rand].image,
+        points: tabnum
+      }
+      res.json(character)
+    })
+});
+
 module.exports = router
